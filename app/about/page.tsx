00e6d405a72e845704f9c1ab73c9b9fa679d34f8
szboynono <szboylbd@hotmail.com>
2023-08-23 23:49:19 -0400

    image added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function About() {
   return (
     <>
@@ -5,10 +7,12 @@ export default function About() {
         <div className="mx-auto max-w-7xl lg:flex lg:justify-between lg:px-8 xl:justify-end">
           <div className="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2">
             <div className="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0">
-              <img
+              <Image
                 className="absolute inset-0 h-full w-full bg-gray-50 object-cover"
-                src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=2560&h=3413&&q=80"
+                src="/work.avif"
                 alt=""
+                width={2000}
+                height={2000}
               />
             </div>
           </div>
