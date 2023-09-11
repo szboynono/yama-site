d02b7f87fc9c8755d96ed85b58233ea27c779532
szboynono <szboylbd@hotmail.com>
2023-09-10 21:58:53 -0400

    done

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -64,7 +64,7 @@ function Services() {
           <Image
             src="/data.jpg"
             alt="Product screenshot"
-            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+            className="h-[750px] rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
             width={2432}
             height={1442}
           />
