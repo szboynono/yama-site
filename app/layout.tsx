ca8797ae3286f87cf6d8a26ab1236789ca049f4a
szboynono <szboylbd@hotmail.com>
2023-09-13 23:07:15 -0400

    more keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,14 +9,37 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Snowmountain Financial Group",
-  description: "Snowmountain Financial Group: Pioneering data analysis and business intelligence for financial institutions. Harness the power of your data to drive decisions and boost profitability.",
+  description:
+    "Snowmountain Financial Group: Pioneering data analysis and business intelligence for financial institutions. Harness the power of your data to drive decisions and boost profitability.",
   robots: "index, follow",
   viewport: "width=device-width, initial-scale=1",
   openGraph: {
     title: "Snowmountain Financial Group",
-    description: "Snowmountain Financial Group: Pioneering data analysis and business intelligence for financial institutions. Harness the power of your data to drive decisions and boost profitability."
+    description:
+      "Snowmountain Financial Group: Pioneering data analysis and business intelligence for financial institutions. Harness the power of your data to drive decisions and boost profitability.",
   },
-  keywords: ['financial services', 'wealth management', 'financial planning', 'stock', 'money management', 'money', 'Data Analysis', 'Data Visualization', 'Statistics']
+  keywords: [
+    "data analytics consulting services",
+    "financial data analysis",
+    "finance management consultant",
+    "business data insights",
+    "financial performance analytics",
+    "data-driven financial strategies",
+    "business intelligence consulting",
+    "financial data visualization",
+    "custom financial analytics solutions",
+    "finance trend analysis",
+    "budget forecasting analytics",
+    "financial data processing",
+    "investment data analytics",
+    "cash flow data analysis",
+    "enterprise financial management consulting",
+    "big data in finance",
+    "financial risk analytics",
+    "profit optimization analytics",
+    "financial KPI analysis",
+    "business growth analytics",
+  ],
 };
 
 export default function RootLayout({
@@ -27,9 +50,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/logo.png" sizes="any" />
+        <link rel="icon" href="/logo.png" sizes="any" />
       </head>
-      <body className={clsx('container mx-auto')}>
+      <body className={clsx("container mx-auto")}>
         <Header />
         {children}
         <Footer />
